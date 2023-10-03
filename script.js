ae2814e0953d814bf6a48635d0acf6664f8c9c96
Matt Jenkins <mattjenkinsww@gmail.com>
2023-10-02 19:28:26 -0600

    fixed the header issues and changed some styling.

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 const menuToggle = document.getElementById('menuToggle')
-const navMenu = document.querySelector('#nav-bar')
-const logo = document.querySelector('.headerImgContainer')
+const mobileMenu = document.querySelector('.mobileMenu')
+const navBar = document.querySelector('.navBar')
+const navBarList = document.getElementById('navBar')
 const header = document.querySelector('header')
+const headerLogo = document.querySelector('.headerImgContainer')
 const closeMenu = document.getElementById('closeMenu')
 
 
+
 menuToggle.addEventListener('click', function () {
-    navMenu.classList.toggle('show-nav-menu')
+    menuToggle.classList.toggle('show-nav-menu')
+    mobileMenu.classList.toggle('show-nav-menu')
+    navBar.classList.toggle('show-nav-menu')
+    navBarList.classList.toggle('show-nav-menu')
     header.classList.toggle('show-nav-menu')
-    logo.classList.toggle('show-nav-menu')
+    headerLogo.classList.toggle('show-nav-menu')
     closeMenu.classList.toggle('show-nav-menu')
 })
 
 closeMenu.addEventListener('click', function () {
-    navMenu.classList.toggle('show-nav-menu')
+    menuToggle.classList.toggle('show-nav-menu')
+    mobileMenu.classList.toggle('show-nav-menu')
+    navBar.classList.toggle('show-nav-menu')
+    navBarList.classList.toggle('show-nav-menu')
     header.classList.toggle('show-nav-menu')
-    logo.classList.toggle('show-nav-menu')
+    headerLogo.classList.toggle('show-nav-menu')
     closeMenu.classList.toggle('show-nav-menu')
 })
